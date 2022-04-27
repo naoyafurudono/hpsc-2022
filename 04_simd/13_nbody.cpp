385d6bd4dc5339cec497566fa0f96fa9591ea8b5
naoyafurudono <furudono.n.aa@m.titech.ac.jp>
2022-04-28 01:07:49 +0900

    done homework for simd

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -1,7 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include <immintrin.h>
 
+  void print(__m256 vec) {
+    float b[8];
+    _mm256_store_ps(b, vec);
+    for(int i=0;i<8;i++)
+	printf("%f ", b[i]);
+    printf("\n");
+  }
+// 小数第二位にforを使った場合と比べて誤差が生じる。
+// TODO 原因を見つける。
 int main() {
   const int N = 8;
   float x[N], y[N], m[N], fx[N], fy[N];
@@ -11,7 +21,67 @@ int main() {
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
+
+  __m256 zero = _mm256_setzero_ps();
+  float buffer[N];
   for(int i=0; i<N; i++) {
+    // rx  = x[i] - x[j];
+    __m256 xivec = _mm256_set1_ps(x[i]);
+    __m256 xvec = _mm256_load_ps(x);
+    __m256 rxvec = _mm256_sub_ps(xivec, xvec);
+
+    // ry  = y[i] - y[j];
+    __m256 yivec = _mm256_set1_ps(y[i]);
+    __m256 yvec = _mm256_load_ps(y);
+    __m256 ryvec = _mm256_sub_ps(yivec, yvec);
+
+    // compute r*r and mask
+    __m256 rtmp = _mm256_mul_ps(rxvec,rxvec);
+    rtmp = _mm256_fmadd_ps(ryvec,ryvec,rtmp);
+    __m256 mask = _mm256_cmp_ps(zero, rtmp, _CMP_NEQ_UQ);
+    //print(rtmp);
+
+    // compute 1/(r*r*r)
+    __m256 rrcpvec = _mm256_rsqrt_ps(rtmp);
+    __m256 rrcp3vec =_mm256_mul_ps(rrcpvec, rrcpvec);
+    rrcp3vec =_mm256_mul_ps(rrcpvec, rrcp3vec);
+
+    __m256 mvec = _mm256_load_ps(m);
+    __m256 minus1 = _mm256_set1_ps(-1);
+
+    __m256 xsubvec = _mm256_mul_ps(rxvec,mvec);
+    xsubvec = _mm256_mul_ps(xsubvec, rrcp3vec);
+    xsubvec = _mm256_mul_ps(xsubvec, minus1);
+    xsubvec = _mm256_blendv_ps(zero, xsubvec, mask);
+    // printf("xsubvec:\n");
+    // print(xsubvec);
+    // print(zero);
+
+    __m256 bvec = _mm256_permute2f128_ps(xsubvec, xsubvec,1);
+    bvec = _mm256_add_ps(bvec, xsubvec);
+    bvec = _mm256_hadd_ps(bvec,bvec);
+    bvec = _mm256_hadd_ps(bvec,bvec);
+    _mm256_store_ps(buffer, bvec);
+    fx[i] += buffer[0];
+
+    __m256 ysubvec = _mm256_mul_ps(ryvec,mvec);
+    ysubvec = _mm256_mul_ps(ysubvec, rrcp3vec);
+    ysubvec = _mm256_mul_ps(ysubvec, minus1);
+    ysubvec = _mm256_blendv_ps(zero, ysubvec, mask);
+
+    bvec = _mm256_permute2f128_ps(ysubvec, ysubvec,1);
+    bvec = _mm256_add_ps(bvec, ysubvec);
+    bvec = _mm256_hadd_ps(bvec,bvec);
+    bvec = _mm256_hadd_ps(bvec,bvec);
+    _mm256_store_ps(buffer, bvec);
+    fy[i] += buffer[0];
+
+    printf("%d %g %g\n",i,fx[i],fy[i]);
+  }
+ /* 
+  printf("ans: \n");
+  for(int i=0; i<N; i++) {
+    fx[i] = fy[i] = 0;
     for(int j=0; j<N; j++) {
       if(i != j) {
         float rx = x[i] - x[j];
@@ -23,4 +93,5 @@ int main() {
     }
     printf("%d %g %g\n",i,fx[i],fy[i]);
   }
+*/
 }
