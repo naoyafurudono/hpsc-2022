7a3df71aa12a846b11e63a155ccbf5f9c2b6a0ed
Naoya Furudono <naoyafurudono@prg.is.titech.ac.jp>
2022-05-19 20:40:15 +0900

    finish homework of io

--- 10_io/17_phdf5_read.cpp
+++ 10_io/17_phdf5_read.cpp
@@ -22,12 +22,13 @@ int main (int argc, char** argv) {
   H5Sget_simple_extent_dims(globalspace, N, NULL);
   hsize_t NX = N[0], NY = N[1];
   hsize_t Nlocal[2] = {NX/dim[0], NY/dim[1]};
+  hsize_t block[2] = {Nlocal[0]/2, Nlocal[1]/2};
   hsize_t offset[2] = {mpirank / dim[0], mpirank % dim[0]};
-  for(int i=0; i<2; i++) offset[i] *= Nlocal[i];
-  hsize_t count[2] = {1,1};
-  hsize_t stride[2] = {1,1};
+  for(int i=0; i<2; i++) offset[i] *= Nlocal[i]/2;
+  hsize_t count[2] = {2,2};
+  hsize_t stride[2] = {block[0]*2,block[1]*2};
   hid_t localspace = H5Screate_simple(2, Nlocal, NULL);
-  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, Nlocal);
+  H5Sselect_hyperslab(globalspace, H5S_SELECT_SET, offset, stride, count, block);
   H5Pclose(plist);
   vector<int> buffer(Nlocal[0]*Nlocal[1]);
   plist = H5Pcreate(H5P_DATASET_XFER);
@@ -48,3 +49,5 @@ int main (int argc, char** argv) {
   printf("N=%d: %lf s (%lf GB/s)\n",NX*NY,time,4*NX*NY/time/1e9);
   MPI_Finalize();
 }
+
+
